d60ca49204a681f16330be135b3bf2424af1a3de
saengle <zohv817@gmail.com>
2022-12-05 16:54:18 +0900

    Add: Pages

--- lib/tab/tab_page.dart
+++ lib/tab/tab_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'account/account_page.dart';
+import 'home/home_page.dart';
+import 'search/search_page.dart';
+
 class TabPage extends StatefulWidget {
   const TabPage({Key? key}) : super(key: key);
 
@@ -10,9 +14,16 @@ class TabPage extends StatefulWidget {
 class _TabPageState extends State<TabPage> {
   int _currentIndex = 0;
 
+  final List _pages = const [
+    AccountPage(),
+    HomePage(),
+    SearchPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {

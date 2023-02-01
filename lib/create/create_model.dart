0fafda7720f028397052075e6c8e8f34a49aa110
saengle <zohv817@gmail.com>
2023-02-01 18:25:52 +0900

    Add: Storage에 사진 업로드, 화면에 표시

--- lib/create/create_model.dart
+++ lib/create/create_model.dart
@@ -2,12 +2,14 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 
 import '../domain/post.dart';
 
 class CreateModel {
   final ImagePicker _picker = ImagePicker();
+  final storage = FirebaseStorage.instance;
 
   Future<File?> getImage() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
@@ -20,6 +22,15 @@ class CreateModel {
   }
 
   Future<void> uploadPost(String title, File imageFile) async {
+    // 이미지 업로드
+    final storageRef = FirebaseStorage.instance.ref();
+    final imageRef = storageRef
+        .child('postImages/${DateTime.now().millisecondsSinceEpoch}.png');
+    // 이미지 url 을 얻기
+    await imageRef.putFile(imageFile);
+
+    final downloadUrl = await imageRef.getDownloadURL();
+    // 게시물 업로드
     final postsRef =
         FirebaseFirestore.instance.collection('posts').withConverter<Post>(
               fromFirestore: (snapshot, _) => Post.fromJson(snapshot.data()!),
@@ -32,8 +43,7 @@ class CreateModel {
         id: newPostRef.id,
         userId: FirebaseAuth.instance.currentUser?.uid ?? '',
         title: title,
-        imageUrl:
-            'http://cdn.ggilbo.com/news/photo/202208/926599_758569_2224.jpg'));
+        imageUrl: downloadUrl));
   }
 }
 // FirebaseAuth.instance.currentUser?.photoURL ??
